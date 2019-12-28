6590843fd62525c9678350c2e09d2e240475d89b
Wzorek <wzorek.piotr@gmail.com>
2019-12-28 22:51:00 +0100

    dev-cnn: Network fixed

--- finetune.py
+++ finetune.py
@@ -5,51 +5,43 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import keras.models as M
 import datetime
-import keras.backend as K
-import keras.utils as U
-import keras.applications as A
-import keras.engine.topology as L
 import matplotlib.pyplot as plt
-from pathlib import Path
-from PIL import Image
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Activation
-from keras.engine.topology import Layer
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.callbacks import ModelCheckpoint
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.imagenet_utils import decode_predictions
 from keras.optimizers import SGD
 
-print(K.tensorflow_backend._get_available_gpus())
-BATCH_SIZE = 32
-
 # Prepare data for network
-data_generator = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.3)
-train_data = data_generator.flow_from_directory(
-                    'input/',
-                    target_size=(256, 256),
-                    subset='training',
-                    shuffle=True,
-                    batch_size=BATCH_SIZE,
-                    class_mode='categorical')
-test_data = data_generator.flow_from_directory(
-                    'input/',
-                    target_size=(256, 256),
-                    subset='validation',
-                    shuffle=True,
-                    batch_size=BATCH_SIZE,
-                    class_mode='categorical')
-print(train_data.class_indices)
+# data_generator = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.3)
+# train_data = data_generator.flow_from_directory(
+#                     'input/',
+#                     target_size=(256, 256),
+#                     subset='training',
+#                     shuffle=True,
+#                     batch_size=32,
+#                     class_mode='categorical')
+# test_data = data_generator.flow_from_directory(
+#                     'input/',
+#                     target_size=(256, 256),
+#                     subset='validation',
+#                     shuffle=True,
+#                     batch_size=32,
+#                     class_mode='categorical')
+# print(train_data.class_indices)
 
 # Build your own ResNet50
 base_model = ResNet50(weights='imagenet', input_shape=(256, 256, 3), include_top=False)
+
+# Fine tune from this layer onwards
+fine_tune_at = 165
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
 base_model.summary()
-base_model.trainable = False
+
 my_model = M.Sequential()
 my_model.add(base_model)
 my_model.add(GlobalAveragePooling2D())
@@ -67,23 +59,22 @@ my_model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accur
 # print("ACC0")
 # print(accuracy0)
 
-checkpoint = ModelCheckpoint('checkpoints\\' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),
-                             monitor='val_acc',
+checkpoint = ModelCheckpoint('./checkpoints/finetune/',
+                             monitor=["val_acc"],
                              verbose=1,
                              mode='max',
                              save_best_only=True)
+log_dir="./logs/finetune/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-log='logs\\resnet\\' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-tensorboard_callback = TensorBoard(log_dir=log, histogram_freq=1)
+callbacks_list = [checkpoint, tensorboard_callback]
 
 learning_process = my_model.fit_generator(train_data,
                                           epochs=100,
                                           validation_data=test_data,
-                                          callbacks=[checkpoint, tensorboard_callback],
-                                          validation_steps=len(test_data)//BATCH_SIZE,
-                                          shuffle=True)
-
+                                          shuffle=True,
+                                          callbacks=callbacks_list)
 plt.figure(1, figsize=(15, 8))
 plt.subplot(2, 2, 1)
 plt.plot(learning_process.history['accuracy'])
